285411e6d7a7b60f9751072f5fcb5213b84de9a8
Murphy Kunder <alisha.kunder.29@gmail.com>
2024-11-03 18:16:52 +0530

    initial commit

---
+++ src/main/java/com/alimurph/postcard/AlimurphPostcardApiApplication.java
@@ -0,0 +1,13 @@
+package com.alimurph.postcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimurphPostcardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimurphPostcardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alimurph/postcard/AlimurphPostcardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimurph.postcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimurphPostcardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
